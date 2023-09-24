84c81e3eac032a886d7e5f45b2eecf26b59bfb44
Aptypp <artur.kucherenko@inkos.games>
2023-09-24 17:31:16 +0300

    Code organization

---
+++ Assets/Scripts/Extensions/MathfExtension.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace UnityTemplateProjects.Extensions
+{
+    public static class MathfExtension
+    {
+        public static void ComputeBarycentricCoordinates(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3, Vector3 point, out float alpha,
+            out float beta, out float gamma)
+        {
+            Vector3 edge1 = vertex2 - vertex1;
+            Vector3 edge2 = vertex3 - vertex1;
+            Vector3 edge3 = vertex3 - vertex2;
+
+            Vector3 toPoint1 = point - vertex1;
+            Vector3 toPoint2 = point - vertex2;
+
+            Vector3 normal = Vector3.Cross(edge1, edge2);
+            float inverseArea = 1.0f / normal.magnitude;
+
+            alpha = Vector3.Cross(toPoint2, edge3).magnitude * inverseArea;
+            beta = Vector3.Cross(toPoint1, edge2).magnitude * inverseArea;
+            gamma = 1.0f - alpha - beta;
+        }
+
+        public static float LinePlaneInterception(in Vector3 point0, in Vector3 point1, Plane plane, out Vector3 interceptionPoint)
+        {
+            Ray rayFromPoint0ToPoint1 = new Ray(point0, point1 - point0);
+
+            plane.Raycast(rayFromPoint0ToPoint1, out float distance);
+            interceptionPoint = rayFromPoint0ToPoint1.GetPoint(distance);
+
+            return distance;
+        }
+
+        public static Vector2 ComputeUvCoordinates(in Vector3 vert0, in Vector3 vert1, in Vector3 vert2, in Vector3 newVert, in Vector2 uv0,
+            in Vector2 uv1, in Vector2 uv2)
+        {
+            MathfExtension.ComputeBarycentricCoordinates(vert0, vert1, vert2, newVert, out float alpha, out float beta, out float gamma);
+
+            return new Vector2(uv0.x * alpha + uv1.x * beta + uv2.x * gamma, uv0.y * alpha + uv1.y * beta + uv2.y * gamma);
+        }
+    }
+}
--- Assets/Scripts/MeshCutting/MeshSlicer.cs
+++ Assets/Scripts/MeshCutting/MeshSlicer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityTemplateProjects.Extensions;
 
 namespace MeshCutting
 {
@@ -56,11 +57,11 @@ namespace MeshCutting
 
                 if (point0Side != point1Side && point0Side != point2Side)
                 {
-                    LinePlaneInterception(vert0, vert1, slicePlane, out Vector3 vert01);
-                    LinePlaneInterception(vert0, vert2, slicePlane, out Vector3 vert02);
+                    MathfExtension.LinePlaneInterception(vert0, vert1, slicePlane, out Vector3 vert01);
+                    MathfExtension.LinePlaneInterception(vert0, vert2, slicePlane, out Vector3 vert02);
 
-                    Vector2 uv01 = ComputeUvCoordinates(vert0, vert1, vert2, vert01, uv0, uv1, uv2);
-                    Vector2 uv02 = ComputeUvCoordinates(vert0, vert1, vert2, vert02, uv0, uv1, uv2);
+                    Vector2 uv01 = MathfExtension.ComputeUvCoordinates(vert0, vert1, vert2, vert01, uv0, uv1, uv2);
+                    Vector2 uv02 = MathfExtension.ComputeUvCoordinates(vert0, vert1, vert2, vert02, uv0, uv1, uv2);
 
                     AddTriangleToMesh(vert01, vert02, vert0, uv01, uv02, uv0, MeshSideExtension.BoolToMeshSide(point0Side));
 
@@ -71,13 +72,13 @@ namespace MeshCutting
                     continue;
                 }
 
-                if (point2Side != point1Side && point2Side != point0Side)
+                if (point2Side != point0Side)
                 {
-                    LinePlaneInterception(vert2, vert0, slicePlane, out Vector3 vert20);
-                    LinePlaneInterception(vert2, vert1, slicePlane, out Vector3 vert21);
+                    MathfExtension.LinePlaneInterception(vert2, vert0, slicePlane, out Vector3 vert20);
+                    MathfExtension.LinePlaneInterception(vert2, vert1, slicePlane, out Vector3 vert21);
 
-                    Vector2 uv20 = ComputeUvCoordinates(vert0, vert1, vert2, vert20, uv0, uv1, uv2);
-                    Vector2 uv21 = ComputeUvCoordinates(vert0, vert1, vert2, vert21, uv0, uv1, uv2);
+                    Vector2 uv20 = MathfExtension.ComputeUvCoordinates(vert0, vert1, vert2, vert20, uv0, uv1, uv2);
+                    Vector2 uv21 = MathfExtension.ComputeUvCoordinates(vert0, vert1, vert2, vert21, uv0, uv1, uv2);
 
                     AddTriangleToMesh(vert20, vert21, vert2, uv20, uv21, uv2, MeshSideExtension.BoolToMeshSide(point2Side));
 
@@ -88,22 +89,17 @@ namespace MeshCutting
                     continue;
                 }
 
-                if (point1Side != point0Side && point1Side != point2Side)
-                {
-                    LinePlaneInterception(vert1, vert0, slicePlane, out Vector3 vert10);
-                    LinePlaneInterception(vert1, vert2, slicePlane, out Vector3 vert12);
+                MathfExtension.LinePlaneInterception(vert1, vert0, slicePlane, out Vector3 vert10);
+                MathfExtension.LinePlaneInterception(vert1, vert2, slicePlane, out Vector3 vert12);
 
-                    Vector2 uv10 = ComputeUvCoordinates(vert0, vert1, vert2, vert10, uv0, uv1, uv2);
-                    Vector2 uv12 = ComputeUvCoordinates(vert0, vert1, vert2, vert12, uv0, uv1, uv2);
+                Vector2 uv10 = MathfExtension.ComputeUvCoordinates(vert0, vert1, vert2, vert10, uv0, uv1, uv2);
+                Vector2 uv12 = MathfExtension.ComputeUvCoordinates(vert0, vert1, vert2, vert12, uv0, uv1, uv2);
 
-                    AddTriangleToMesh(vert1, vert12, vert10, uv1, uv12, uv10, MeshSideExtension.BoolToMeshSide(point1Side));
+                AddTriangleToMesh(vert1, vert12, vert10, uv1, uv12, uv10, MeshSideExtension.BoolToMeshSide(point1Side));
 
-                    AddTriangleToMesh(vert10, vert12, vert2, uv10, uv12, uv2, MeshSideExtension.BoolToMeshSide(!point1Side));
+                AddTriangleToMesh(vert10, vert12, vert2, uv10, uv12, uv2, MeshSideExtension.BoolToMeshSide(!point1Side));
 
-                    AddTriangleToMesh(vert10, vert2, vert0, uv10, uv2, uv0, MeshSideExtension.BoolToMeshSide(!point1Side));
-
-                    continue;
-                }
+                AddTriangleToMesh(vert10, vert2, vert0, uv10, uv2, uv0, MeshSideExtension.BoolToMeshSide(!point1Side));
             }
 
             List<Mesh> newMeshes = new List<Mesh>();
@@ -142,32 +138,6 @@ namespace MeshCutting
             return newMeshes.ToArray();
         }
 
-        private Vector2 ComputeUvCoordinates(in Vector3 vert0, in Vector3 vert1, in Vector3 vert2, in Vector3 newVert, in Vector2 uv0, in Vector2 uv1,
-            in Vector2 uv2)
-        {
-            ComputeBarycentricCoordinates(vert0, vert1, vert2, newVert, out float alpha, out float beta, out float gamma);
-
-            return new Vector2(uv0.x * alpha + uv1.x * beta + uv2.x * gamma, uv0.y * alpha + uv1.y * beta + uv2.y * gamma);
-        }
-
-        public void ComputeBarycentricCoordinates(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3, Vector3 point, out float alpha, out float beta,
-            out float gamma)
-        {
-            Vector3 edge1 = vertex2 - vertex1;
-            Vector3 edge2 = vertex3 - vertex1;
-            Vector3 edge3 = vertex3 - vertex2;
-
-            Vector3 toPoint1 = point - vertex1;
-            Vector3 toPoint2 = point - vertex2;
-
-            Vector3 normal = Vector3.Cross(edge1, edge2);
-            float triangleArea = normal.magnitude / 2.0f;
-
-            alpha = Vector3.Cross(toPoint2, edge3).magnitude / (2.0f * triangleArea);
-            beta = Vector3.Cross(toPoint1, edge2).magnitude / (2.0f * triangleArea);
-            gamma = 1.0f - alpha - beta;
-        }
-
         private void AddTriangleToMesh(in Vector3 vert0, in Vector3 vert1, in Vector3 vert2, in Vector2 uv0, in Vector2 uv1, in Vector2 uv2,
             MeshSide meshSide)
         {
@@ -193,16 +163,6 @@ namespace MeshCutting
             uvs.Add(uv0);
             uvs.Add(uv1);
             uvs.Add(uv2);
-        }
-
-        private float LinePlaneInterception(in Vector3 point0, in Vector3 point1, Plane plane, out Vector3 interceptionPoint)
-        {
-            Ray rayFromPoint0ToPoint1 = new Ray(point0, point1 - point0);
-
-            plane.Raycast(rayFromPoint0ToPoint1, out float distance);
-            interceptionPoint = rayFromPoint0ToPoint1.GetPoint(distance);
-
-            return distance;
         }
     }
 }
